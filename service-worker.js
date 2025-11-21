f0e844e8829cc8de781c55551eb89d8a4f142a1e
printedidea3d-max <printed.idea3d@gmail.com>
2025-11-21 11:14:19 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+
+const CACHE_NAME = "karaoke-paty-modern-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./songs.json",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES_TO_CACHE)));
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(resp=>resp || fetch(e.request))
+  );
+});
